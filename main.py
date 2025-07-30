e4bd05c5366bd2b15cb7d39fe21a48c154dd064c
VIKAS  KUMAR <vikaskumariit964954@gmail.com>
2025-07-31 00:03:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 # from keras.models import load_model
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 
 from PIL import Image
 import numpy as np
